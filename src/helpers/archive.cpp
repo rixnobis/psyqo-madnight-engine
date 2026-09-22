rixnobis/psyqo-madnight-engine

commit 31654e2b9bde2c0710d97b9909cbff52b829e9b1

refactor!: updates to bring the engine to v0.0.1 (#48)
* chore: switch to `#pragma once`

* refactor(gameobjects): rework constructor to be private with new params
Makes `GameObjectManager` a friend

* refactor(gameobject): use `string_view` for `GetGameObjectByName`

* chore(gameobject): format with tabs

* chore(gameobject): format with tabs

* refactor(gameobject): switch `SetMesh` and `SetTexture` to `const eastl::string_view&`

* chore(mesh): format

* chore(texture): format

* refactor(animation): swap out uses of `const char*` for `const eastl::string_view&`

* refactor(cdrom): swap out uses of `const char*` for `const eastl::string_view&`

* refactor(modsound): swap out uses of `const char*` for `const eastl::string_view&`

* refactor(gameplay-hud): swap out uses of `const char*` for `const eastl::string_view&`

* refactor(hud-element): swap out uses of `const char*` for `const eastl::string_view&`

* refactor(sprite-hud-element): swap out uses of `const char*` for `const eastl::string_view&`

* refactor(text-hud-element): swap out uses of `const char*` for `const eastl::string_view&`

* refactor(menu-item): swap out uses of `const char*` for `const eastl::string_view&`

* refactor(menu): swap out uses of `const char*` for `const eastl::string_view&`

* refactor(quaternion): switch to full fat `int32_t`

* refactor(particles): make constructors private with friends

* chore: cleanup redundant uses of `c_str()`

* refactor(scene-loader): cleanup use of `fixed_string` in function signature

* refactor(billboard): make constructors private with friends, tidy up `fixed_string` usage

* fix(billboard): it was protected for a reason, oops

* chore: update psyqo

* refactor(controllerhelper): remove `IsPadAnalog` in favour of psyqo's version

* refactor(collision): swap out `psyqo::SoftMath` for `psyqo::GteMath`

* refactor(gameobject,raycast): swap out `psyqo::SoftMath` for `psyqo::GteMath`

src/helpers/archive.cpp
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2025-2026 Matt Hadden / Madnight Games
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
 #ifdef PCDRV
 #include "archive.hh"
 #else
@@ -5,15 +11,8 @@
 #endif
 #include "../madnight.hh"
 
-#include <psyqo/xprintf.h>
 #include <psyqo-paths/archive-manager.hh>
-
-#ifdef PCDRV
-psyqo::CDRomPCDrv ArchiveHelper::m_cdrom(g_gameIsoName);
-#endif
-psyqo::paths::ArchiveManager ArchiveHelper::m_archiveManager;
-bool ArchiveHelper::m_archiveManagerInit = false;
-char ArchiveHelper::m_loadingFileName[MAX_ARCHIVE_FILE_NAME_LEN];
+#include <psyqo/xprintf.h>
 
 void ArchiveHelper::init(eastl::function<void()> cb) {
 	m_archiveManager.registerUCL_NRV2EDecompressor();
@@ -23,34 +22,35 @@ void ArchiveHelper::init(eastl::function<void()> cb) {
 #else
 	auto& cdrom = m_cdrom;
 #endif
-	
-    m_archiveManager.initialize(23, cdrom, [cb](bool success) {
+
+	m_archiveManager.initialize(23, cdrom, [cb, this](bool success) {
 		m_archiveManagerInit = success;
 		printf("ARCHIVE: Initialize result=%d\n", m_archiveManagerInit);
 		cb();
 	});
 }
 
-psyqo::Coroutine<psyqo::Buffer<uint8_t>> ArchiveHelper::LoadFile(const char *fileName) {
+psyqo::Coroutine<psyqo::Buffer<uint8_t>> ArchiveHelper::LoadFile(const eastl::string_view& fileName) {
 	if (m_archiveManagerInit) {
 		printf("ARCHIVE: Attempting to read %s...\n", fileName);
 
 #ifndef PCDRV
-	auto& cdrom = CDRomHelper::CDRomDevice();
+		auto& cdrom = CDRomHelper::CDRomDevice();
 #else
-	auto& cdrom = m_cdrom;
-#endif		
+		auto& cdrom = m_cdrom;
+#endif
 
 #ifdef PCDRV
 		auto buffer = co_await m_archiveManager.readFile(fileName, cdrom);
 #else
 		auto buffer = co_await m_archiveManager.readFile(fileName, cdrom);
 #endif
-		if (buffer.empty())
+		if (buffer.empty()) {
 			printf("ARCHIVE: File %s not found or empty\n", fileName);
-		else
+		} else {
 			printf("ARCHIVE: Read %d bytes\n", buffer.size());
-		
+		}
+
 		co_return eastl::move(buffer);
 	} else {
 		printf("ARCHIVE: Manager not initialized.\n");

src/helpers/cdrom.cpp
@@ -1,21 +1,18 @@
+/*
+ * Copyright (C) 2025-2026 Matt Hadden / Madnight Games
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
 #include "cdrom.hh"
-#include "psyqo/alloc.h"
-#include "psyqo/coroutine.hh"
-#include "psyqo/xprintf.h"
-#include "../render/renderer.hh"
+#include "../madnight.hh"
 #include "archive.hh"
+#include <psyqo/alloc.h>
+#include <psyqo/coroutine.hh>
+#include <psyqo/xprintf.h>
 
-#ifndef PCDRV
-
-psyqo::CDRomDevice CDRomHelper::m_cdrom;
-psyqo::ISO9660Parser CDRomHelper::m_isoParser = psyqo::ISO9660Parser(&m_cdrom);
-psyqo::paths::CDRomLoader CDRomHelper::m_cdromLoader;
-char CDRomHelper::m_loadingFileName[32];
-
-#else
-
+#ifdef PCDRV
 #include "common/kernel/pcdrv.h"
-
 #endif
 
 void CDRomHelper::init(eastl::function<void()> cb) {
@@ -26,27 +23,28 @@ void CDRomHelper::init(eastl::function<void()> cb) {
 	PCinit();
 #endif
 
-  ArchiveHelper::init(cb);
+	g_madnightEngine.m_archiveHelper.init(cb);
 }
 
-psyqo::Coroutine<psyqo::Buffer<uint8_t>> CDRomHelper::LoadFile(const char *fileName) {
+psyqo::Coroutine<psyqo::Buffer<uint8_t>> CDRomHelper::LoadFile(const eastl::string_view& fileName) {
 #ifndef PCDRV
 	get_iso_file_name(fileName, m_loadingFileName);
 	printf("CDRom: Attempting to read %s...\n", m_loadingFileName);
 
 	auto buffer = co_await m_cdromLoader.readFile(m_loadingFileName, m_isoParser);
-	if (buffer.empty())
+	if (buffer.empty()) {
 		printf("CDRom: File %s not found or empty\n", m_loadingFileName);
-	else
+	} else {
 		printf("CDRom: Read %d bytes\n", buffer.size());
-	
-        co_return eastl::move(buffer);
+	}
+
+	co_return eastl::move(buffer);
 #else
 	printf("PCDrv: Attempting to read %s...\n", fileName);
 
 	psyqo::Buffer<uint8_t> buffer;
 
-	int fd = PCopen(fileName, 0, 0);
+	int fd = PCopen(fileName.data(), 0, 0);
 	if (fd < 0) {
 		printf("PCDrv: Failed to open %s\n", fileName);
 		co_return eastl::move(buffer);
@@ -76,7 +74,7 @@ psyqo::Coroutine<psyqo::Buffer<uint8_t>> CDRomHelper::LoadFile(const char *fileN
 }
 
 #ifndef PCDRV
-void CDRomHelper::get_iso_file_name(const char *file_name, char *iso_filename) {
+void CDRomHelper::get_iso_file_name(const char* file_name, char* iso_filename) {
 	snprintf(iso_filename, 32, "%s;1", file_name);
 }
 #endif

src/helpers/cdrom.hh
@@ -1,28 +1,35 @@
+/*
+ * Copyright (C) 2025-2026 Matt Hadden / Madnight Games
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
 #pragma once
 
-#include <stdint.h>
-#include "psyqo/coroutine.hh"
-#include "psyqo/buffer.hh"
+#include <EASTL/string_view.h>
+#include <psyqo/buffer.hh>
+#include <psyqo/coroutine.hh>
 
 #ifndef PCDRV
+#include "psyqo-paths/cdrom-loader.hh"
 #include "psyqo/cdrom-device.hh"
 #include "psyqo/iso9660-parser.hh"
-#include "psyqo-paths/cdrom-loader.hh"
 #endif
 
-class CDRomHelper final
-{
-public:
-    static void init(eastl::function<void()> cb);
-    static psyqo::Coroutine<psyqo::Buffer<uint8_t>> LoadFile(const char *fileName);
+[[deprecated("Replaced with `ArchiveHelper`")]];
+class CDRomHelper final {
+  public:
+	void init(eastl::function<void()> cb);
+	psyqo::Coroutine<psyqo::Buffer<uint8_t>> LoadFile(const eastl::string_view& fileName);
 
 #ifndef PCDRV
-    static psyqo::CDRomDevice& CDRomDevice() { return m_cdrom; }
-private:
-    static psyqo::CDRomDevice m_cdrom;
-    static psyqo::ISO9660Parser m_isoParser;
-    static psyqo::paths::CDRomLoader m_cdromLoader;
-    static char m_loadingFileName[32];
-    static void get_iso_file_name(const char *file_name, char *iso_filename);
+	psyqo::CDRomDevice& CDRomDevice() { return m_cdrom; }
+
+  private:
+	psyqo::CDRomDevice m_cdrom;
+	psyqo::ISO9660Parser m_isoParser = psyqo::ISO9660Parser(&m_cdrom);
+	psyqo::paths::CDRomLoader m_cdromLoader;
+	char m_loadingFileName[32];
+	void get_iso_file_name(const char* file_name, char* iso_filename);
 #endif
 };

src/madnight.hh
@@ -1,54 +1,102 @@
+/*
+ * Copyright (C) 2025-2026 Matt Hadden / Madnight Games
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
 #pragma once
+#include "animation/animation_manager.hh"
+#include "controller/controller.hh"
+#include "core/billboard/billboard_manager.hh"
+#include "core/collision.hh"
+#include "core/debug/debug_menu.hh"
+#include "core/debug/perf_monitor.hh"
+#include "core/object/gameobject_manager.hh"
+#include "core/particles/particle_manager.hh"
+#include "core/raycast.hh"
+#include "file_loader.hh"
+#include "helpers/archive.hh"
+#include "helpers/cdrom.hh"
 #include "helpers/load_queue.hh"
-#include "psyqo/scene.hh"
+#include "mesh/colbin_manager.hh"
+#include "mesh/mesh_manager.hh"
+#include "mesh/skeleton/skeleton.hh"
 #include "rand.hh"
+#include "sound/mod_sound_manager.hh"
+#include "sound/sound_manager.hh"
+#include "textures/texture_manager.hh"
 
-#include "psyqo/coroutine.hh"
-#include "psyqo/application.hh"
-#include "psyqo/trigonometry.hh"
-#include "psyqo/advancedpad.hh"
-#include "EASTL/vector.h"
+#include <EASTL/vector.h>
+#include <psyqo/advancedpad.hh>
+#include <psyqo/application.hh>
+#include <psyqo/coroutine.hh>
+#include <psyqo/scene.hh>
+#include <psyqo/trigonometry.hh>
 
-class MadnightEngine final : public psyqo::Application
-{
-    void prepare() override;
+class MadnightEngine final : public psyqo::Application {
+	void prepare() override;
 
-    psyqo::Coroutine<> m_initialLoadRoutine;
-public:
-    psyqo::Trig<> m_trig;
-    psyqo::AdvancedPad m_input;
+	psyqo::Coroutine<> m_initialLoadRoutine;
+	CDRomHelper m_cdromHelper;
 
-    // make sure you seed this with a nice number when starting your first scene
-    // using `g_madnightEngine.gpu().now()` for example.
-    Rand m_rand;
+  public:
+	psyqo::Trig<> m_trig;
+	psyqo::AdvancedPad m_input;
 
-    // you probably want to use hardloadingscreen below, but if you have no files to load then no need, just use this instead
-    // this will pop the existing scene unless you specify `true` for keepPreviousscene
-    psyqo::Scene* SwitchScene(psyqo::Scene *scene, bool keepPrevious = false);
+	// managers that will aid in asset management
+	TextureManager m_textureManager;
+	MeshManager m_meshManager;
+	GameObjectManager m_gameObjectManager;
+	ColbinManager m_colbinManager;
+	SoundManager m_soundManager;	   // this allows you to use VAG files via SPU
+	ModSoundManager m_modSoundManager; // this is for the MOD player
+	AnimationManager m_animationManager;
+	BillboardManager m_billboardManager;
+	ParticleEmitterManager m_particleEmitterManager;
+	FileLoader m_fileLoader;	   // loading screens use this to load a queue of files from an archive
+	ArchiveHelper m_archiveHelper; // used for loading a single file from an archive
 
-    // shows a loading screen and unloads all known meshes and textures
-    // this will also unload the current scene, and start the requested scene fresh, so if you have
-    // stuff that occurs in startscene, you need to make sure you don't do it again
-    // as this will trigger a `Start` reason, and not `Resume`
-    psyqo::Coroutine<> HardLoadingScreen(eastl::vector<LoadQueue>&& files, psyqo::Scene* postLoadScene);
+	// helpers for various things like controller, collisions, etc.
+	ControllerHelper m_controllerHelper;
+	Collision m_collisionHelper;
+	Raycast m_raycast;
+	SkeletonController m_skeletonController;
 
-    // show your custom loading screen and unloads all known meshes, textures, etc.
-    // on top of that the current scene is unloaded and replaced with what is specified in `loadingScene`
-    psyqo::Coroutine<> HardLoadingScreen(eastl::vector<LoadQueue>&& files, psyqo::Scene* loadingScene, psyqo::Scene* postLoadScene);
+	DebugMenu m_debugMenu;
+	PerfMonitor m_perfMonitor;
 
-    // shows a loading screen but keeps your current scene on the stack, which will be returned to once loading is done
-    // this will not unload any data from pools such as meshes, textures, game objects, etc.
-    psyqo::Coroutine<> SoftLoadingScreen(eastl::vector<LoadQueue>&& files);
-    // shows your custom loading screen but keeps your current scene on the stack, which will be returned to once loading is done
-    // this will not unload any data from pools such as meshes, textures, game objects, etc.
-    psyqo::Coroutine<> SoftLoadingScreen(eastl::vector<LoadQueue>&& files, psyqo::Scene* loadingScene);
+	// make sure you seed this with a nice number when starting your first scene
+	// using `g_madnightEngine.gpu().now()` for example.
+	Rand m_rand;
 
-    /*
-     * this is the very first thing that is called once the engine is initialized and ready
-     * for use. you can do whatever you want with this here. for example you can make use of `co_await HardLoadingScreen` in
-     * this function before you do whatever you need/want to do in your actual game code.
-     */
-    psyqo::Coroutine<> InitialLoad(void);
+	// you probably want to use hardloadingscreen below, but if you have no files to load then no need, just use this
+	// instead this will pop the existing scene unless you specify `true` for keepPreviousscene
+	psyqo::Scene* SwitchScene(psyqo::Scene* scene, bool keepPrevious = false);
+
+	// shows a loading screen and unloads all known meshes and textures
+	// this will also unload the current scene, and start the requested scene fresh, so if you have
+	// stuff that occurs in startscene, you need to make sure you don't do it again
+	// as this will trigger a `Start` reason, and not `Resume`
+	psyqo::Coroutine<> HardLoadingScreen(eastl::vector<LoadQueue>&& files, psyqo::Scene* postLoadScene);
+
+	// show your custom loading screen and unloads all known meshes, textures, etc.
+	// on top of that the current scene is unloaded and replaced with what is specified in `loadingScene`
+	psyqo::Coroutine<> HardLoadingScreen(eastl::vector<LoadQueue>&& files, psyqo::Scene* loadingScene,
+										 psyqo::Scene* postLoadScene);
+
+	// shows a loading screen but keeps your current scene on the stack, which will be returned to once loading is done
+	// this will not unload any data from pools such as meshes, textures, game objects, etc.
+	psyqo::Coroutine<> SoftLoadingScreen(eastl::vector<LoadQueue>&& files);
+	// shows your custom loading screen but keeps your current scene on the stack, which will be returned to once
+	// loading is done this will not unload any data from pools such as meshes, textures, game objects, etc.
+	psyqo::Coroutine<> SoftLoadingScreen(eastl::vector<LoadQueue>&& files, psyqo::Scene* loadingScene);
+
+	/*
+	 * this is the very first thing that is called once the engine is initialized and ready
+	 * for use. you can do whatever you want with this here. for example you can make use of `co_await
+	 * HardLoadingScreen` in this function before you do whatever you need/want to do in your actual game code.
+	 */
+	psyqo::Coroutine<> InitialLoad(void);
 };
 
 // Trap into the resident debugger with the exit code in $a0 via break
@@ -57,16 +105,10 @@ public:
 // the program (Unirom reports HLTD) and leaves the exit code readable in
 // $a0, giving the host a deterministic end-of-binary signal instead of a
 // printed sentinel string. On the emulator pcsx_exit() has already exited,
-// so this is never reached there.    
+// so this is never reached there.
 static inline void exitBreak(int code) {
-    register int a0 asm("$4") = code;
-    __asm__ volatile("break 4, 0\n" : : "r"(a0) : "memory");
+	register int a0 asm("$4") = code;
+	__asm__ volatile("break 4, 0\n" : : "r"(a0) : "memory");
 }
 
 extern MadnightEngine g_madnightEngine;
-
-// PCDRV debug builds (-pcdrv) mount an ISO from your host filesystem instead
-// of real CD-ROM hardware. Each game must define this exactly once,
-// somewhere in its own source (not the engine's) — e.g.:
-//     const char* g_gameIsoName = "ssweep.iso";
-extern const char* g_gameIsoName;

commit c5e36f068ee9d83b285eb336dc049a3ae01d5c11

ci: build the engine and boot getting-started (#1)
* fix(cdrom): restore the non-PCDRV build broken by #48

`CDRomHelper` stopped being static but `ArchiveHelper` still called it as if
it were, `cdrom.cpp` lost its renderer include, and `get_iso_file_name` kept
a `const char*` parameter after its caller moved to `string_view`.

* ci: build the library and getting-started, and boot it in pcsx-redux

* ci: skip credential persistence, nugget breaks its recursive foreach

* ci: init nugget by hand, checkout recurses into its broken xmake-psx gitlink

.github/workflows/build.yml
@@ -1,0 +1,58 @@
+name: Build
+
+on:
+  push:
+  pull_request:
+  workflow_dispatch:
+
+concurrency:
+  group: ${{ github.workflow }}-${{ github.ref }}
+  cancel-in-progress: true
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    container: ghcr.io/grumpycoders/pcsx-redux-build:latest
+
+    steps:
+      - uses: actions/checkout@v4
+
+      - name: Submodules
+        # Not through checkout: nugget carries a third_party/xmake-psx gitlink
+        # with no .gitmodules entry, and checkout recurses into it.
+        run: |
+          git config --global --add safe.directory "$GITHUB_WORKSPACE"
+          git submodule update --init third_party/nugget
+
+      - name: Library, PCDRV
+        run: |
+          make -j2 DEFINES=PCDRV
+          make clean
+
+      - name: Library
+        run: |
+          make -j2
+          ls -l libmadnight-engine.a
+
+      - name: Getting started
+        # The scaffold expects the engine as a submodule named madnight_engine.
+        run: |
+          ln -s .. getting-started/madnight_engine
+          make -C getting-started -j2
+          ls -l getting-started/build/madnight-engine-game.ps-exe
+
+      - name: pcsx-redux
+        # --appimage-extract rather than mounting it: the runner has no FUSE.
+        run: |
+          curl -sL -o pcsx-redux.zip https://distrib.app/pub/org/pcsx-redux/project/dev-linux-x64/latest
+          unzip -q pcsx-redux.zip
+          ./PCSX-Redux-*.AppImage --appimage-extract > /dev/null
+
+      - name: Boot the getting started executable
+        # pcsx-redux brings SDL video up even under -testmode.
+        run: xvfb-run tools/boot-demo.sh "$GITHUB_WORKSPACE/squashfs-root/AppRun"
+
+      - uses: actions/upload-artifact@v4
+        with:
+          name: madnight-engine-game.ps-exe
+          path: getting-started/build/madnight-engine-game.ps-exe

src/helpers/archive.cpp
@@ -18,7 +18,7 @@ void ArchiveHelper::init(eastl::function<void()> cb) {
 	m_archiveManager.registerUCL_NRV2EDecompressor();
 
 #ifndef PCDRV
-	auto& cdrom = CDRomHelper::CDRomDevice();
+	auto& cdrom = g_madnightEngine.m_cdromHelper.CDRomDevice();
 #else
 	auto& cdrom = m_cdrom;
 #endif
@@ -35,7 +35,7 @@ psyqo::Coroutine<psyqo::Buffer<uint8_t>> ArchiveHelper::LoadFile(const eastl::st
 		printf("ARCHIVE: Attempting to read %s...\n", fileName);
 
 #ifndef PCDRV
-		auto& cdrom = CDRomHelper::CDRomDevice();
+		auto& cdrom = g_madnightEngine.m_cdromHelper.CDRomDevice();
 #else
 		auto& cdrom = m_cdrom;
 #endif

src/helpers/cdrom.cpp
@@ -6,6 +6,7 @@
 
 #include "cdrom.hh"
 #include "../madnight.hh"
+#include "../render/renderer.hh"
 #include "archive.hh"
 #include <psyqo/alloc.h>
 #include <psyqo/coroutine.hh>
@@ -74,7 +75,7 @@ psyqo::Coroutine<psyqo::Buffer<uint8_t>> CDRomHelper::LoadFile(const eastl::stri
 }
 
 #ifndef PCDRV
-void CDRomHelper::get_iso_file_name(const char* file_name, char* iso_filename) {
-	snprintf(iso_filename, 32, "%s;1", file_name);
+void CDRomHelper::get_iso_file_name(const eastl::string_view& file_name, char* iso_filename) {
+	snprintf(iso_filename, 32, "%.*s;1", int(file_name.size()), file_name.data());
 }
 #endif

src/helpers/cdrom.hh
@@ -30,6 +30,6 @@ class CDRomHelper final {
 	psyqo::ISO9660Parser m_isoParser = psyqo::ISO9660Parser(&m_cdrom);
 	psyqo::paths::CDRomLoader m_cdromLoader;
 	char m_loadingFileName[32];
-	void get_iso_file_name(const char* file_name, char* iso_filename);
+	void get_iso_file_name(const eastl::string_view& file_name, char* iso_filename);
 #endif
 };

src/madnight.hh
@@ -34,6 +34,8 @@
 #include <psyqo/trigonometry.hh>
 
 class MadnightEngine final : public psyqo::Application {
+	friend class ArchiveHelper;
+
 	void prepare() override;
 
 	psyqo::Coroutine<> m_initialLoadRoutine;

tools/boot-demo.sh
@@ -1,0 +1,55 @@
+#!/bin/sh
+#
+# Boots the getting-started executable under pcsx-redux and waits for the
+# engine to hand control to the game code.
+#
+#   make -C getting-started
+#   tools/boot-demo.sh /path/to/pcsx-redux
+#
+# There is no disc, so the archive fails to mount; the engine carries on to
+# MadnightGame::InitialLoad regardless, and that is what this waits for.
+
+set -e
+
+EMU=${1:?usage: $0 <pcsx-redux binary>}
+ROOT=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
+EXE=$ROOT/getting-started/build/madnight-engine-game.ps-exe
+TOKEN="welcome to your game code!"
+
+if [ ! -f "$EXE" ]; then
+    echo "$EXE is missing; build it with make -C getting-started" >&2
+    exit 1
+fi
+
+WORK=$(mktemp -d)
+trap 'rm -rf "$WORK"' EXIT
+
+# The game loop never exits on its own, so the emulator is killed once the
+# token shows up, and the timeout caps a run that never gets there. stdbuf
+# because the log is polled while the emulator is still running.
+timeout 120 stdbuf -oL -eL "$EMU" -no-ui -testmode -run -stdout \
+        -loadexe "$EXE" > "$WORK/log" 2>&1 &
+PID=$!
+
+found=0
+i=0
+while [ $i -lt 110 ]; do
+    if grep -q "$TOKEN" "$WORK/log"; then
+        found=1
+        break
+    fi
+    kill -0 $PID 2> /dev/null || break
+    sleep 1
+    i=$((i + 1))
+done
+
+kill $PID 2> /dev/null || true
+wait $PID 2> /dev/null || true
+cat "$WORK/log"
+
+if [ $found -ne 1 ]; then
+    echo "game code never started" >&2
+    exit 1
+fi
+
+echo "game code started after ${i}s"